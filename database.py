a12dad516829181a80b2428a26283dc851f30236
96hoshi <m.locascio96@hotmail.com>
2022-05-07 21:02:16 +0200

    updated database and added handler

--- database.py
+++ database.py
@@ -1,11 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import Session
 
 from model import Base, User, Device
 
+class DBHandler:
+    def __init__(self):
+        self.engine = create_engine("sqlite:///plant.db", echo=True, future=True)
+        # if not database_exists(self.engine.url):
+        #     create_database(self.engine.url)
+        Base.metadata.create_all(self.engine)
+        self.session = Session(self.engine)
+        print("db-handler")
 
-engine = create_engine("sqlite://", echo=True, future=True)
-if not database_exists(engine.url):
-    create_database(engine.url)
+    def add_User(self, name, chat_id):
+        new_user = User()
+        new_user.name = name
+        new_user.chat_id = chat_id
 
-Base.metadata.create_all(engine)
+        self.session.add(new_user)
+        self.session.commit()
--- model.py
+++ model.py
@@ -7,26 +7,25 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "user_account"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(30))
-    chat_id = Column(String)
+    chat_id = Column(String(100), unique=True)
 
-    device = relationship(
-        "Device", back_populates="user", cascade="all, delete-orphan"
-    )
+    # device = relationship(
+    #     "Device", back_populates="user", cascade="all, delete-orphan"
+    # )
 
     def __repr__(self):
         return f"User(id={self.id!r}, name={self.name!r}, device={self.device!r})"
 
-class Device(Base):
+class Device(Base)
     __tablename__ = "device"
 
     id = Column(Integer, primary_key=True)
-    hum = Column(Integer)
-    temp = Column(Integer)
-    water = Column(Integer)
+    topic = Column(String(30))
+    value = Column(Integer)
     timestamp = Column(DateTime)
 
-    user = relationship("User", back_populates="device")
+    # user = relationship("User", back_populates="device")
     def __repr__(self):
         return f"Device(id={self.id!r}, timestamp={self.timestamp!r})"
